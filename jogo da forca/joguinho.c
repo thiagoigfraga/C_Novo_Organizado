a9b7b8d2af12df243e692aab884eb6401ae5ed52
Thiago Garcia <49455222+thiagoigfraga@users.noreply.github.com>
2019-10-01 14:13:18 -0300

    Update joguinho.c

--- jogo da forca/joguinho.c
+++ jogo da forca/joguinho.c
@@ -1,1 +1,56 @@
+#include <stdio.h>
+#include <string.h>
 
+int main() {
+  
+  //variaveis//
+
+    char palavrasecreta[20];
+    sprintf(palavrasecreta, "MELANCIA");
+
+    int acertou = 0;
+    int enforcou = 0;
+
+    char chutes[26];
+    int tentativas = 0;
+  
+  
+  //jogo//
+
+    do {
+
+        for(int i = 0; i < strlen(palavrasecreta); i++) //inicia loop que varre toda a string//
+        {
+            int achou = 0;
+          
+
+            for(int j = 0; j < tentativas; j++) {
+                if(chutes[j] == palavrasecreta[i]) {
+                    achou = 1;
+                    break;
+                }
+            }
+          
+          
+            if(achou) {
+                printf("%c ", palavrasecreta[i]);
+            } else {
+                printf("_ ");
+            }
+        }
+      
+      
+      printf("\n");
+
+        char chute;
+      
+        printf("Qual letra? ");
+        scanf(" %c", &chute);
+
+        chutes[tentativas] = chute;
+        tentativas++;
+
+
+    } while (!acertou && !enforcou);
+
+}
