197da23069afb9d8ae5e55f6b7f2a9932733db02
Thiago Garcia <49455222+thiagoigfraga@users.noreply.github.com>
2019-10-01 12:19:03 -0300

    Update jogomelhorado.c

--- jogodaadivinhacao/jogomelhorado.c
+++ jogodaadivinhacao/jogomelhorado.c
@@ -1,4 +1,55 @@
-for(i = 1; i <= tentativas; i++)
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main()
+{
+    //mecanica logica variaveis//
+
+    srand(time(0));
+    int numero_grande = rand();
+    int chute;
+    int numero_secreto = numero_grande % 101;
+    int tentativas;
+    int nivel;
+    int i;
+    double pontos = 1000;
+    int maior;
+    int menor;
+    int acertou;
+    int derrota;
+
+    //jogo//
+
+    printf("********Bem vindo ao jogo da adivinhação!********\n");
+    printf("Selecione o nível:\n");
+    printf("|1 - FÁCIL | 2 - MÉDIO | 3 - DIFÍCIL|\n");
+    scanf("%d", &nivel);
+
+    switch (nivel)
+    {
+    case (1):
+        tentativas = 15;
+        printf("Você selecionou o modo fácil com %d tentativas!\n", tentativas);
+        break;
+    case (2):
+        tentativas = 10;
+        printf("Você selecionou o modo médio com %d tentativas!\n", tentativas);
+        break;
+    case (3):
+        tentativas = 5;
+        printf("Você selecionou o modo difícil com %d tentativas!\n", tentativas);
+        break;
+    
+    default:
+        tentativas = 1000;
+        printf("Você selecionou o modo randômico com %d tentativas!\n", tentativas);
+
+
+        break;
+    }
+
+    for(i = 1; i <= tentativas; i++)
     {
         
         printf("Tentativa %d de %d\n", i,tentativas);
